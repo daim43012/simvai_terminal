5c53d316e420fdd94573e762f0c367c0dcb51218
daim43012 <vgatsko.21@gmail.com>
2025-03-11 22:52:09 +0200

    init project

---
+++ main.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+app.whenReady().then(() => {
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadURL('http://localhost:5173'); // Загружаем Svelte UI
+});
